cb5bc995c286940dafe62a152e7f48009c995fdd
Peter Nadel <peter.m.nadel@gmail.com>
2026-01-04 15:38:28 -0500

    debug

--- site/pages/2_Characters.py
+++ site/pages/2_Characters.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import inject_css
 
 inject_css()
-with open("../mkdwn/characters.md", "r") as f:
+with open("/mount/src/wintertide-2026/site/mkdwn/characters.md", "r") as f:
     markd = f.read()
 
 st.markdown(markd)
