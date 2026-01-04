71bc21db16ceed63c78f37a7c1da5f294a102539
Peter Nadel <peter.m.nadel@gmail.com>
2026-01-04 15:28:11 -0500

    debug

--- site/pages/1_Background.py
+++ site/pages/1_Background.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from utils import inject_css
+import os
 
 inject_css()
+
+st.write(os.listdir(".."))
+
 with open("../mkdwn/background.md", "r") as f:
     markd = f.read()
 
